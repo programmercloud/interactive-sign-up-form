23f3c13a23f1d2e3e9c7a3775726f9127c921087
programmercloud <88435015+programmercloud@users.noreply.github.com>
2022-05-14 15:54:23 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$('.email').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.envelop').addClass("next");
+    } else {
+      $('.envelop').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.email').click(
+  function(){
+    console.log("Something");
+    $('.email-section').addClass("fold-up");
+    $('.password-section').removeClass("folded");
+  }
+);
+
+$('.password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.lock').addClass("next");
+    } else {
+      $('.lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.password').click(
+  function(){
+    console.log("Something");
+    $('.password-section').addClass("fold-up");
+    $('.repeat-password-section').removeClass("folded");
+  }
+);
+
+$('.repeat-password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.repeat-lock').addClass("next");
+    } else {
+      $('.repeat-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button.repeat-password').click(
+  function(){
+    console.log("Something");
+    $('.repeat-password-section').addClass("fold-up");
+    $('.success').css("marginTop", 0);
+  }
+);
+
